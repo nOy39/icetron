27827d4bb4b4cc72b46a42550b77bb6e8d4d5273
Andrey <popov.a83@bk.ru>
2018-08-14 11:21:05 +0200

    empty project

---
+++ src/main/java/org/a2lpo/icetron/IcetronApplication.java
@@ -0,0 +1,12 @@
+package org.a2lpo.icetron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcetronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcetronApplication.class, args);
+	}
+}
---
+++ src/test/java/org/a2lpo/icetron/IcetronApplicationTests.java
@@ -0,0 +1,16 @@
+package org.a2lpo.icetron;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IcetronApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
